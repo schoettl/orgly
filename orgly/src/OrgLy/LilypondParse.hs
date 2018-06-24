aa8f94870a1360938e1e71a80969df84761d5d4e
Jakob Schöttl <jschoett@gmail.com>
2018-06-24 10:46:02 +0200

    Bugfix in parser

--- orgly/src/OrgLy/LilypondParse.hs
+++ orgly/src/OrgLy/LilypondParse.hs
@@ -9,30 +9,26 @@ import Data.Attoparsec.Text
 import qualified Data.Music.Lilypond as L
 import Data.Char (isAlpha, isSpace)
 import Control.Applicative ((<|>))
+import Control.Monad (when)
+import Data.List (nub)
 
 data Command =
     PlainCommand Text
   | MusicCommand L.Music
-  | Stuff
+  deriving Show
 
 parseLilypondSource :: Parser [L.Music]
 parseLilypondSource = do
-  cmds <- many' $ choice
-    [ parseStuff
-    , parseCommand
-    ]
+  cmds <- many' (skipWhile (/='\\') >> parseCommand)
   return $ foldr f [] cmds
   where
     f (MusicCommand m) ms = m:ms
     f _ ms = ms
 
-parseStuff :: Parser Command
-parseStuff = skipWhile (/='\\') >> return Stuff
-
 parseCommand :: Parser Command
 parseCommand = do
   char '\\'
-  parsePlainCommand <|> parseMusicCommand
+  parseMusicCommand <|> parsePlainCommand
 
 parsePlainCommand :: Parser Command
 parsePlainCommand = do
@@ -55,8 +51,8 @@ parseKey = do
 
 parsePitch :: Parser L.Pitch
 parsePitch = do
-  -- TODO nur für einfache tonarten - fis dur geht noch nicht :(
-  -- auch schwierig: deutscher/englischer modus
+  -- TODO auch schwierig: deutscher/englischer modus
+  -- aktuell: engischer modus, aber h-dur funktioniert auch
   c <- letter
   let pitchName = case c of
         'c' -> L.C
@@ -67,7 +63,27 @@ parsePitch = do
         'a' -> L.A
         'b' -> L.B
         'h' -> L.B
-  return $ L.Pitch (pitchName, 0, 0)
+  accidentals <- many' (string "es" <|> string "is")
+  when ((length accidentals > 2) || ((length.nub) accidentals > 1)) $
+    fail "invalid accidental prefix to pitch name"
+  let accidental = toAccidental accidentals
+  octaves <- many' (char ',' <|> char '\'')
+  when ((length octaves > 5) || ((length.nub) octaves > 1)) $
+    fail "invalid octave prefix to pitch"
+  let octave = toOctave octaves
+  return $ L.Pitch (pitchName, accidental, octave)
+
+toAccidental :: [Text] -> L.Accidental
+toAccidental as
+  | null as = 0
+  | head as == "es" = - length as
+  | head as == "is" =   length as
+
+toOctave :: [Char] -> L.Octaves
+toOctave os
+  | null os = 0
+  | head os == ','  = - length os
+  | head os == '\'' =   length os
 
 parseMode :: Parser L.Mode
 parseMode = (string "\\major" >> return L.Major)
